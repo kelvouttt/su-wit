3890225e5090096b429367eb880fcbf7258ef160
Kelvin <kelvin0898@live.com>
2021-11-14 21:07:47 +1100

    fixing the button not printing properly in the window

--- new_js.js
+++ new_js.js
@@ -2,14 +2,14 @@ const container = document.querySelector(".container")
 
 const content = document.createElement('div')
 content.classList.add('result')
-
+// container.appendChild(content)
 
 
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => button.addEventListener('click', playGame));
 
 function computerPlay() {
-  let responses = ["batu", "kertas", "gunting"];
+  let responses = ["rock", "paper", "scissor"];
   response = responses[Math.floor(Math.random() * responses.length)];
   return response;
   // console.log(response)
@@ -21,16 +21,16 @@ function playGame(e) {
   playerResponse = e.target.className;
   computerSelection = computerPlay()  
   console.log(playerResponse)
-    console.log(computerSelection)
-  for (let i = userScore; i < 5; i++){
-    if (playerResponse == 'gunting' && computerSelection == 'kertas') {
+  // console.log(computerSelection)
+  // for (let i = userScore; i < 5; i++){
+    if (playerResponse == 'scissor' && computerSelection == 'paper') {
       // console.log("It's a tie!")
       content.textContent = "You win! Scissor beats paper"
-    } else if (playerResponse == "kertas" && computerSelection == "batu") {
+    } else if (playerResponse == "paper" && computerSelection == "rock") {
       userScore++
       // console.log("You win, scissor beats paper")
       content.textContent = "You win! Paper beats rock"
-    } else if (playerResponse == "batu" && computerSelection == "gunting") {
+    } else if (playerResponse == "rock" && computerSelection == "scissor") {
       userScore++
       // console.log("You win, rock beats scissor")
       content.textContent = "You win! Rock beats scissor"
@@ -40,12 +40,12 @@ function playGame(e) {
     } else {
       computerScore++
       // console.log("You lose, try again!")
-      content.texContent = "You lose, try again!!"
+      content.textContent = "You lose, try again!!"
     }
-    container.appendChild(content)
-  }
-  console.log(computerScore)
-  console.log(userScore)
+    container.appendChild(content);
+  // }
+  // console.log(computerScore)
+  // console.log(userScore)
 }
 
 
